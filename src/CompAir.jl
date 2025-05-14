f3144e2934b1db8e33013bdd527d28f5d319d875
Seawhan Lee <seawhanl@gmail.com>
2025-05-15 00:46:26 +0900

    Includes core module files for main package
    Enables integration of atmospheric and shock calculation modules by including key source files, preparing the package for expanded functionality and further development.

--- src/CompAir.jl
+++ src/CompAir.jl
@@ -2,4 +2,13 @@ module CompAir
 
 # Write your package code here.
 
+include("atmos1976.jl")
+include("cone_shock.jl")
+include("isentropic.jl")
+include("normal_shock.jl")
+include("nozzle.jl")
+include("oblique_shcok.jl")
+include("prandtl_expand.jl")
+
+
 end
---
+++ src/isentropic.jl
@@ -0,0 +1,57 @@
+# Isentropic relation (Anderson Book 8.4x)
+
+"""
+마하수 M 일때 정체 온도비
+
+Parameters
+----------
+M : float
+    마하수
+gamma : float, optional
+    비열비
+
+Returns
+--------
+t0 : 정체 온도비
+"""
+function t0_t(M, gamma=1.4)
+    return 1 + 0.5 * (gamma - 1) * M^2
+end
+
+"""
+마하수 `M` 일때 정체 압력비
+
+Parameters
+----------
+M : float
+    마하수
+gamma : float, optional
+    비열비
+
+Returns
+--------
+p0 : 정체 압력비
+"""
+function p0_p(M, gamma=1.4)
+    t = t0_t(M, gamma)
+    return t^(gamma / (gamma - 1))
+end
+
+"""
+마하수 `M` 일때 정체 밀도비
+
+Parameters
+----------
+M : float
+    마하수
+gamma : float, optional
+    비열비
+
+Returns
+--------
+rho0 : 정체 밀도비
+"""
+function rho0_rho(M, gamma=1.4)
+    t = t0_t(M, gamma)
+    return t^(1 / (gamma - 1))
+end
