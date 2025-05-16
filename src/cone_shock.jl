6da25462e1a9fcb525b4e7824dc845fc71de977a
Seawhan Lee <seawhanl@gmail.com>
2025-05-17 01:10:04 +0900

    Refactors cone shock solvers and improves interface
    Streamlines and corrects function interfaces for cone shock calculations, consolidating logic for solving properties on the cone surface. Improves parameter clarity, fixes variable naming inconsistencies, and enhances code maintainability. Enables more flexible usage by allowing explicit specification of flow and ray angles.

--- src/cone_shock.jl
+++ src/cone_shock.jl
@@ -8,7 +8,7 @@ function _taylor_maccoll(theta, y, gamma=1.4)
     v_r, v_theta = y
     dydt = [
         v_theta;
-        (v_theta^2 * v_r - (gamma - 1) / 2 * (1 - v_r^2 - v_theta^2) * (2 * v_r + v_theta / tan(theta))) 
+        (v_theta^2 * v_r - (gamma - 1) / 2 * (1 - v_r^2 - v_theta^2) * (2 * v_r + v_theta / tan(theta)))
         / ((gamma - 1) / 2 * (1 - v_r^2 - v_theta^2) - v_theta^2)
     ]
 
@@ -18,7 +18,7 @@ end
 function _integrate_tm(M, angle, theta, gamma=1.4)
     theta_max_val = theta_max(M, gamma)
 
-    if theta <= theta_max
+    if theta <= theta_max_val
         beta = oblique_beta_weak(M, theta, gamma)
         M2 = oblique_mach2(M, theta, gamma)
     else
@@ -26,9 +26,9 @@ function _integrate_tm(M, angle, theta, gamma=1.4)
         M2 = normal_mach2(M, gamma)
     end
 
-    v = sqrt(((gamma-1)/2*M2^2)/(1+(gamma-1)/2*M2^2))
-    v_theta = -v*sin(deg2rad(beta-theta))
-    v_r = v*cos(deg2rad(beta-theta))
+    v = sqrt(((gamma - 1) / 2 * M2^2) / (1 + (gamma - 1) / 2 * M2^2))
+    v_theta = -v * sin(deg2rad(beta - theta))
+    v_r = v * cos(deg2rad(beta - theta))
 
     # Integrate over [beta, angle]
     tspan = (deg2rad(beta), deg2rad(angle))
@@ -57,7 +57,7 @@ theta_eff : float
     형상 각도 (degree)
 """
 function theta_eff(M, angle, gamma=1.4)
-    f(x) = _integrate_tm(M, angle, x, gamma).y[end,end]
+    f(x) = _integrate_tm(M, angle, x, gamma).y[end, end]
     return find_zero(f, 1e-3, Newton())
 end
 
@@ -110,9 +110,9 @@ function _cone_mach(M, angle, theta, gamma)
     vec = _integrate_tm(M, angle, theta, gamma).y[:, end]
 
     v = norm(vec)
-    phi = angle + rad2deg(atan(vec[2]/vec[1]))
+    phi = angle + rad2deg(atan(vec[2] / vec[1]))
 
-    return sqrt(2/(gamma-1)*(v^2 / (1-v^2))), phi
+    return sqrt(2 / (gamma - 1) * (v^2 / (1 - v^2))), phi
 end
 
 """
@@ -133,7 +133,7 @@ M2 : float
     경사 충격파후 마하수
 """
 function cone_mach(M, angle, gamma=1.4)
-    theta, _ = theta_eff(M, angle, gamma)
+    theta = theta_eff(M, angle, gamma)
     return _cone_mach(M, angle, theta, gamma)
 end
 
@@ -193,6 +193,54 @@ p0ratio : float
 beta : float
     경사 충격파 각도 (degree)
 """
-function solve_cone(M, angle, gamma=1.4)
-    mc, rhoc, pc, p0ratio, beta, _ = solve()
+function solve_cone(M, angle; gamma=1.4)
+    mc, rhoc, pc, p0ratio, beta, _ = solve(M, angle, angle, gamma=gamma)
+    return mc, rhoc, pc, p0ratio, beta
+end
+
+"""
+마하수 ``M``, Cone 반각 ``\\theta`` 일때 ray 각도 ``\\psi`` 발생한 Cone 표면에서 물성치
+
+Parameters
+----------
+M : float
+    충격파 전 마하수
+angle : float
+    쇄기 각도 (degree)
+psi : float
+    Ray 각도 (degree)
+gamma : float, optional
+    비열비
+
+Returns
+--------
+mcone : float
+    콘 표면 마하수
+rho2 : float
+    콘 표면 밀도비
+p2 : float
+    콘 표면 압력비
+p0ratio : float
+    콘 표면 전압력비
+beta : float
+    경사 충격파 각도 (degree)
+phi : float
+        유동 방향 (degree)
+"""
+function solve(M, angle, psi; gamma=1.4)
+    theta = theta_eff(M, angle, gamma=gamma)
+    
+    # 충격파 전/후 물성치 계산
+    m2, rho2, p2, p0ratio, beta = obq.solve(M, theta, gamma=gamma)
+    
+    # Cone 마하수, 속도 방향
+    mc, phi = _Mcone(M, psi, theta, gamma=gamma)
+    
+    # 밀도
+    rhoc = rho2*isen.rho0_rho(m2, gamma=gamma)/isen.rho0_rho(mc, gamma=gamma)
+    
+    # 압력
+    pc = p2*isen.p0_p(m2, gamma=gamma) / isen.p0_p(mc, gamma=gamma)
+    
+    return mc, rhoc, pc, p0ratio, beta, phi
 end
