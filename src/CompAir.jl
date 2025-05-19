08f84faf0a09d293295218f01f5438db6a36e56c
Seawhan Lee <seawhanl@gmail.com>
2025-05-20 01:50:21 +0900

    Refactors exports in `CompAir.jl` to include additional functions for normal and oblique shock calculations, Prandtl-Meyer expansion, and cone flow analysis, enhancing module usability.

--- src/CompAir.jl
+++ src/CompAir.jl
@@ -10,7 +10,11 @@ include("nozzle.jl")
 include("oblique_shock.jl")
 include("prandtl_expand.jl")
 
-# Export main functions from each module
-export atmos1976, cone_shock, isentropic, normal_shock, nozzle, oblique_shock, prandtl_expand
+export t0_over_t, p0_over_p, rho0_over_rho
+export normal_mach2, rho2_over_rho1, p2_over_p1, t2_over_t1, normal_p02, p02_over_p01, solve_normal
+export theta_beta, oblique_beta_weak, theta_max, Mn1, oblique_mach2, solve_oblique
+export prandtl_meyer, expand_mach2, expand_p2, theta_p
+export theta_eff, cone_beta_weak, cone_mach2, cone_mach, solve_shock, solve_cone_theta, solve_cone_phi
+export mdot, area_ratio_at, mach_by_area_ratio, me6, pe6, me5, pe5
 
 end
