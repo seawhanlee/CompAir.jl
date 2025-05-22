1d69566707be05498f4078724483adff92d3e6c5
Seawhan Lee <seawhanl@gmail.com>
2025-05-22 15:09:23 +0900

    수정: solve_cone_theta를 solve_cone_properties로 변경

--- src/CompAir.jl
+++ src/CompAir.jl
@@ -14,7 +14,7 @@ export t0_over_t, p0_over_p, rho0_over_rho
 export normal_mach2, rho2_over_rho1, p2_over_p1, t2_over_t1, normal_p02, p02_over_p01, solve_normal
 export theta_beta, oblique_beta_weak, theta_max, Mn1, oblique_mach2, solve_oblique
 export prandtl_meyer, expand_mach2, expand_p2, theta_p
-export theta_eff, cone_beta_weak, cone_mach2, cone_mach_surface, solve_shock, solve_cone_theta, solve_cone_phi
+export theta_eff, cone_beta_weak, cone_mach2, cone_mach_surface, solve_shock, solve_cone_properties
 export mdot, area_ratio_at, mach_by_area_ratio, me6, pe6, me5, pe5
 export atmos1976_at
 
